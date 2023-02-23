3ff0b4e1c75537e29dbd21e5e6b5b9d9fa1a0836
MassiveSnake <stam.bart94@gmail.com>
2023-02-23 14:19:03 +0100

    commit from terminal
    commit

--- Hour_Registry_env/Hour_Registry_Run.py
+++ Hour_Registry_env/Hour_Registry_Run.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
     MainWindow = MyMainWindow()
-    #MainWindow.resize_table_to_contents()
     MainWindow.show()
     sys.exit(app.exec_())
